1ca0e060ae80ea0e6e456eed3fb1df97f49f9ce7
dolamore <antonbalkouski@gmail.com>
2025-07-21 14:30:07 +0400

    Initial commit

---
+++ src/main/java/com/test/motivationletterbot/MotivationLetterBotApplication.java
@@ -0,0 +1,13 @@
+package com.test.motivationletterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivationLetterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotivationLetterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/motivationletterbot/MotivationLetterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.motivationletterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivationLetterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
