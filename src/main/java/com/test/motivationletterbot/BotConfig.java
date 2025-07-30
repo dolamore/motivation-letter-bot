f06e89b2f6153bd43e0e48c0550f5893d5c92e95
dolamore <antonbalkouski@gmail.com>
2025-07-30 07:58:36 +0400

    feat: add bot creator id

--- src/main/java/com/test/motivationletterbot/BotConfig.java
+++ src/main/java/com/test/motivationletterbot/BotConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -34,5 +36,10 @@ public class BotConfig {
         });
         executor.initialize();
         return executor;
+    }
+
+    @Bean
+    public TelegramClient telegramClient(BotProperties botProperties) {
+        return new OkHttpTelegramClient(botProperties.getToken());
     }
 }
