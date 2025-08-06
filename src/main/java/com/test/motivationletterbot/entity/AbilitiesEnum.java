b63655a7b006190de73d3018d4ee33d67883c80a
dolamore <antonbalkouski@gmail.com>
2025-08-06 15:03:45 +0400

    feat: add keyboard to user session

--- src/main/java/com/test/motivationletterbot/entity/AbilitiesEnum.java
+++ src/main/java/com/test/motivationletterbot/entity/AbilitiesEnum.java
@@ -15,80 +15,84 @@ import static com.test.motivationletterbot.entity.BotMenuStateEnum.*;
 @Getter
 public enum AbilitiesEnum {
     START_ABILITY(
-            MAIN.getStateCommands(),
             "start",
             "Start motivation message creation",
+            MAIN.getStateCommands(),
             UserSession::startSession,
-            UserSession::returnMessage,
-            InlineKeyboards::startKeyboard
+            UserSession::startingMessage,
+            UserSession::startKeyboard
     ),
     MENU_ABILITY(
-            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "menu",
             "Show main menu",
+            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::startSession,
             UserSession::returnMessage,
-            InlineKeyboards::startKeyboard
+            UserSession::startKeyboard
     ),
     START_MOTIVATION_ABILITY(
-            EnumSet.of(END_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "start_m",
             "Write new motivation",
+            EnumSet.of(END_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::startMotivationWriting,
             UserSession::returnMessage,
             null
     ),
     CONTINUE_MOTIVATION_ABILITY(
-            EnumSet.of(END_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "continue_m",
             "Continue writing motivation",
+            EnumSet.of(END_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::startMotivationWriting,
             UserSession::returnMessage,
             null
     ),
     RECORD_MOTIVATION_ABILITY(
-            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "end_m",
             "End motivation writing",
+            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::completeMotivation,
             UserSession::returnMessage,
             null
     ),
     END_MOTIVATION_ABILITY(
-            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "end_m",
             "End motivation writing",
+            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::completeMotivation,
             UserSession::returnMessage,
             null
     ),
     START_ROLE_DESCRIPTION_ABILITY(
-            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "start_rd",
             "Start role description writing",
+            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::resetVacancy,
             UserSession::returnMessage,
             null
     ),
     END_ROLE_DESCRIPTION_ABILITY(
-            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             "end_rd",
             "End role description writing",
+            EnumSet.of(START_MOTIVATION_COMMAND, START_ROLE_DESCRIPTION_COMMAND, START_COMMAND),
             UserSession::completeVacancy,
             UserSession::returnMessage,
             null
     );
 
-    private final List<BotCommand> commands;
     private final String abilityName;
     private final String info;
+    private final List<BotCommand> commands;
     private final Consumer<UserSession> sessionAction;
     private final Function<UserSession, String> message;
-    private final Function<InlineKeyboards, List<InlineKeyboardRow>> inlineKeyboardSupplier;
+    private final Function<UserSession, List<InlineKeyboardRow>> inlineKeyboardSupplier;
 
-    AbilitiesEnum(EnumSet<CommandsEnum> commands, String abilityName, String info,
-                  Consumer<UserSession> sessionAction, Function<UserSession, String> message,
-                  Function<InlineKeyboards, List<InlineKeyboardRow>> inlineKeyboardSupplier) {
+    AbilitiesEnum(
+            String abilityName,
+            String info,
+            EnumSet<CommandsEnum> commands,
+            Consumer<UserSession> sessionAction,
+            Function<UserSession, String> message,
+            Function<UserSession, List<InlineKeyboardRow>> inlineKeyboardSupplier) {
         this.commands = commands.stream().map(CommandsEnum::getBotCommand).toList();
         this.abilityName = abilityName;
         this.info = info;
