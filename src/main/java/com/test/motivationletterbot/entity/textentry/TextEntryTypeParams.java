94738ec9932931aaa618104b80a8eb3a78bc13dc
dolamore <antonbalkouski@gmail.com>
2025-08-08 13:19:36 +0400

    feat: add ismandatory feeld to text entries

--- src/main/java/com/test/motivationletterbot/entity/textentry/TextEntryTypeParams.java
+++ src/main/java/com/test/motivationletterbot/entity/textentry/TextEntryTypeParams.java
@@ -6,8 +6,8 @@ import static com.test.motivationletterbot.entity.keyboard.KeyboardRowEnum.*;
 
 public record TextEntryTypeParams(InlineKeyboardRow keyboardRow,
                                   InlineKeyboardRow submitKeyboardRow,
-                                  String continueAbilityKey) {
-    public static final TextEntryTypeParams MOTIVATION_TEXT_ENTRY_PARAMS = new TextEntryTypeParams(MOTIVATION_ROW.getRow(), SUBMIT_MOTIVATION_ROW.getRow(), "cont_m");
-    public static final TextEntryTypeParams VACANCY_TEXT_ENTRY_PARAMS = new TextEntryTypeParams(ROLE_DESCRIPTION_ROW.getRow(), SUBMIT_ROLE_DESCRIPTION_ROW.getRow(), "cont_rd");
-    public static final TextEntryTypeParams ADDITIONAL_INFORMATION_TEXT_ENTRY_PARAMS = new TextEntryTypeParams(ADDITIONAL_INFORMATION_ROW.getRow(), SUBMIT_ADDITIONAL_INFORMATION_ROW.getRow(), "cont_ai");
+                                  String continueAbilityKey, boolean isMandatory) {
+    public static final TextEntryTypeParams MOTIVATION_TEXT_ENTRY_PARAMS = new TextEntryTypeParams(MOTIVATION_ROW.getRow(), SUBMIT_MOTIVATION_ROW.getRow(), "cont_m", false);
+    public static final TextEntryTypeParams VACANCY_TEXT_ENTRY_PARAMS = new TextEntryTypeParams(ROLE_DESCRIPTION_ROW.getRow(), SUBMIT_ROLE_DESCRIPTION_ROW.getRow(), "cont_rd", true);
+    public static final TextEntryTypeParams ADDITIONAL_INFORMATION_TEXT_ENTRY_PARAMS = new TextEntryTypeParams(ADDITIONAL_INFORMATION_ROW.getRow(), SUBMIT_ADDITIONAL_INFORMATION_ROW.getRow(), "cont_ai", false);
 }
