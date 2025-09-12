27590229708a4887bf710c84c5aec26dbacc6fde
dolamore <antonbalkouski@gmail.com>
2025-09-12 15:46:49 +0200

    fix: user promot generation

--- src/main/java/com/test/motivationletterbot/llm/OpenAiClientService.java
+++ src/main/java/com/test/motivationletterbot/llm/OpenAiClientService.java
@@ -6,11 +6,13 @@ import com.openai.client.okhttp.OpenAIOkHttpClient;
 import com.openai.models.ChatModel;
 import com.openai.models.chat.completions.ChatCompletion;
 import com.openai.models.chat.completions.ChatCompletionCreateParams;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Slf4j
 public final class OpenAiClientService extends AbstractLlmClient {
     private final OpenAIClient client;
     private final ChatModel model;
@@ -28,6 +30,9 @@ public final class OpenAiClientService extends AbstractLlmClient {
     }
 
     public String sendPrompt(String userPrompt) {
+        log.warn("Sending prompt to OpenAI: {}", userPrompt);
+        log.warn("System prompt: {}", getSystemPrompt());
+
         ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
                 .model(model)
                 .addSystemMessage(getSystemPrompt())
